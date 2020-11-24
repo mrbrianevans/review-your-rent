8d82e954fbf957b8c84558542d3b8be4cc0bc383
Brian Evans <ebrian101@gmail.com>
2020-11-24 09:48:57 +0000

    Add addresses to database on search
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- frontend/scripts/components/SearchBar.tsx
+++ frontend/scripts/components/SearchBar.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
-// import {promisify} from 'util'
+import {IAddress} from "../types/IAddress";
+import firebase from "firebase";
+
 
 // the geographical location to bias search results towards
 const LOCATION: google.maps.LatLng = new google.maps.LatLng(50.73790969615289, -3.535053172263085)
 // the distance from the above location (3km)
 const RADIUS: number = 3_000
+const CITY_NAME: string = "Exeter"
+const COUNTRY_NAME: string = "UK"
 
 export const SearchBar: () => JSX.Element = () => {
 
@@ -15,7 +19,7 @@ export const SearchBar: () => JSX.Element = () => {
                 tempSearchPrediction(newQuery.target.value)
 
             }
-        } className={"search-box"} autoFocus/>
+        } className={"search-box"}/>
     )
 }
 
@@ -44,15 +48,48 @@ export const SearchBar: () => JSX.Element = () => {
 const tempSearchPrediction: (query: string) => void = (query) => {
     const autocomplete: google.maps.places.AutocompleteService = new google.maps.places.AutocompleteService
     const autocompletions: string[] = []
-    const queryRequest = {input: query, location: LOCATION, radius: RADIUS}
+    const queryRequest = {input: query, location: LOCATION, radius: RADIUS, types: ['address']}
+    const returnedAddresses: IAddress[] = []
     autocomplete.getPlacePredictions(
-        {input: query, location: LOCATION, radius: RADIUS},
+        queryRequest,
         (predictions, status) => {
             if (status !== google.maps.places.PlacesServiceStatus.OK) console.error(status)
-            else predictions.forEach(prediction => {
-                console.log(`Prediction: ${prediction.structured_formatting.main_text}`)
-                autocompletions.push(prediction.structured_formatting.main_text)
-            })
+            else {
+                predictions.forEach(prediction => {
+
+                    // console.log(`\nPrediction: ${prediction.structured_formatting.main_text}, secondary text: ${prediction.structured_formatting.secondary_text}`)
+                    // console.log(`Terms: ${JSON.stringify(prediction.terms)}`)
+                    // console.log(`Types: ${JSON.stringify(prediction.types)}`)
+                    // console.log(`Distance: ${JSON.stringify(prediction.distance_meters)}`)
+                    // console.log(`Reference: ${JSON.stringify(prediction.reference)}`)
+                    // console.log(`ID: ${JSON.stringify(prediction.place_id)}\n`)
+
+                    if (prediction.terms[prediction.terms.length - 1].value === COUNTRY_NAME
+                        && prediction.terms[prediction.terms.length - 2].value === CITY_NAME) {
+                        console.log("This matches the criteria to be entered into the database\n\n\n")
+                        let address: IAddress = {
+                            "street-address": prediction.structured_formatting.main_text,
+                            "secondary-address": prediction.structured_formatting.secondary_text,
+                            reference: prediction.place_id,
+                            terms: prediction.terms.map((predictionTerm) => predictionTerm.value)
+                        }
+                        console.log(address)
+                        returnedAddresses.push(address)
+                    }
+                    autocompletions.push(prediction.structured_formatting.main_text)
+                })
+                addAddressesToDatabase(returnedAddresses)
+            }
             return autocompletions
         })
+}
+
+
+const addAddressesToDatabase = (addresses: IAddress[]) => {
+    const housesRef = firebase.database().ref("/houses")
+    addresses.forEach(address => {
+        housesRef.child(address["street-address"]).set(address)
+            .then(() => console.log(`Added ${address["street-address"]} to database`))
+        // set the houseReference to the address object in the database
+    })
 }
---
+++ frontend/scripts/types/IAddress.ts
@@ -0,0 +1,6 @@
+export interface IAddress {
+    'street-address': string,
+    'secondary-address': string,
+    'reference': string,
+    'terms': { [key: number]: string }
+}
