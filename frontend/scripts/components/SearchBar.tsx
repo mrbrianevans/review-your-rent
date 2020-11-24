8c715bd0c59e8c05fe9d9f753effd17d4bca05fc
Brian Evans <ebrian101@gmail.com>
2020-11-24 18:32:46 +0000

    Search address suggestions #2
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- frontend/scripts/components/SearchBar.tsx
+++ frontend/scripts/components/SearchBar.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
-import {IAddress} from "../types/IAddress";
-import firebase from "firebase";
-
+import * as React from "react"
+import {useState} from "react"
+import {IAddress} from "../types/IAddress"
+import firebase from "firebase/app"
+import 'firebase/database'
+import {Link} from 'react-router-dom'
 
 // the geographical location to bias search results towards
 const LOCATION: google.maps.LatLng = new google.maps.LatLng(50.73790969615289, -3.535053172263085)
@@ -11,77 +13,63 @@ const CITY_NAME: string = "Exeter"
 const COUNTRY_NAME: string = "UK"
 
 export const SearchBar: () => JSX.Element = () => {
-
-    return (
-        <input type={"text"} placeholder={"Street address"} inputMode={"search"} onChange={
-            (newQuery) => {
-                console.log("Now searching for property " + newQuery.target.value)
-                tempSearchPrediction(newQuery.target.value)
-
-            }
-        } className={"search-box"}/>
-    )
-}
-
-// const searchPrediction: (query: string) => Promise<string[]> = (query) => {
-//     // if(google===undefined){
-//     //     console.log("You need to import Google")
-//     //     return false
-//     // }
-//     const autocomplete: google.maps.places.AutocompleteService = new google.maps.places.AutocompleteService
-//     // const autocompletions:string[] = []
-//     const queryRequest = { input: query, location: LOCATION, radius: RADIUS }
-//     const autocompletePromise = promisify<google.maps.places.AutocompletionRequest,
-//         (google.maps.places.AutocompletePrediction[])>(autocomplete.getPlacePredictions)
-//
-//     return autocompletePromise(queryRequest)
-//         .then((predictions, status)=>{
-//             const autocompletions:string[] = []
-//             predictions.forEach(prediction=>{
-//                 console.log(`Prediction: ${prediction.structured_formatting.main_text}`)
-//                 autocompletions.push(prediction.structured_formatting.main_text)
-//             })
-//             return autocompletions
-//         })
-// }
-
-const tempSearchPrediction: (query: string) => void = (query) => {
-    const autocomplete: google.maps.places.AutocompleteService = new google.maps.places.AutocompleteService
-    const autocompletions: string[] = []
-    const queryRequest = {input: query, location: LOCATION, radius: RADIUS, types: ['address']}
-    const returnedAddresses: IAddress[] = []
-    autocomplete.getPlacePredictions(
-        queryRequest,
-        (predictions, status) => {
-            if (status !== google.maps.places.PlacesServiceStatus.OK) console.error(status)
-            else {
-                predictions.forEach(prediction => {
-
-                    // console.log(`\nPrediction: ${prediction.structured_formatting.main_text}, secondary text: ${prediction.structured_formatting.secondary_text}`)
-                    // console.log(`Terms: ${JSON.stringify(prediction.terms)}`)
-                    // console.log(`Types: ${JSON.stringify(prediction.types)}`)
-                    // console.log(`Distance: ${JSON.stringify(prediction.distance_meters)}`)
-                    // console.log(`Reference: ${JSON.stringify(prediction.reference)}`)
-                    // console.log(`ID: ${JSON.stringify(prediction.place_id)}\n`)
-
-                    if (prediction.terms[prediction.terms.length - 1].value === COUNTRY_NAME
-                        && prediction.terms[prediction.terms.length - 2].value === CITY_NAME) {
-                        console.log("This matches the criteria to be entered into the database\n\n\n")
-                        let address: IAddress = {
-                            "street-address": prediction.structured_formatting.main_text,
-                            "secondary-address": prediction.structured_formatting.secondary_text,
-                            reference: prediction.place_id,
-                            terms: prediction.terms.map((predictionTerm) => predictionTerm.value)
+    const [suggestions, setSuggestions] = useState<IAddress[]>()
+    const getAddressSuggestions: (query: string) => void = (query) => {
+        query = query.trim()
+        if (!query.match(/[0-9].*/)) return
+        const autocomplete: google.maps.places.AutocompleteService = new google.maps.places.AutocompleteService
+        const autocompletions: IAddress[] = []
+        const queryRequest = {input: query, location: LOCATION, radius: RADIUS, types: ['address']}
+        autocomplete.getPlacePredictions(
+            queryRequest,
+            (predictions, status) => {
+                if (status !== google.maps.places.PlacesServiceStatus.OK) console.error(status)
+                else {
+                    predictions.forEach(prediction => {
+                        if (prediction.terms[prediction.terms.length - 1].value === COUNTRY_NAME
+                            && prediction.terms[prediction.terms.length - 2].value === CITY_NAME) {
+                            let address: IAddress = {
+                                "street-address": prediction.structured_formatting.main_text,
+                                "secondary-address": prediction.structured_formatting.secondary_text,
+                                reference: prediction.place_id,
+                                terms: prediction.terms.map((predictionTerm) => predictionTerm.value)
+                            }
+                            autocompletions.push(address)
+                        } else {
+                            // console.log("Invalid address discarded")
                         }
-                        console.log(address)
-                        returnedAddresses.push(address)
+                    })
+                }
+                setSuggestions(autocompletions)
+            })
+    }
+    return (
+        <div>
+            <div id={"search-box-wrapper"}>
+                <input type={"text"} placeholder={"Street address"} inputMode={"search"} onChange={
+                    (newQuery) => {
+                        console.log("Now searching for property " + newQuery.target.value)
+                        getAddressSuggestions(newQuery.target.value)
+
                     }
-                    autocompletions.push(prediction.structured_formatting.main_text)
-                })
-                addAddressesToDatabase(returnedAddresses)
-            }
-            return autocompletions
-        })
+                } className={"search-box"}/>
+            </div>
+            <div id={"search-suggestions"}>
+                {suggestions?.map((suggestion, index) =>
+                    <span key={index} className={"search-suggestion"} onClick={
+                        () => {
+                            addAddressToDatabase(suggestion)
+                        }
+                    }>
+                        <Link to={`/property/${suggestion["street-address"]}`}>
+                            {suggestion["street-address"]}
+                        </Link>
+                    </span>
+                )}
+            </div>
+        </div>
+
+    )
 }
 
 
@@ -92,4 +80,10 @@ const addAddressesToDatabase = (addresses: IAddress[]) => {
             .then(() => console.log(`Added ${address["street-address"]} to database`))
         // set the houseReference to the address object in the database
     })
+}
+
+const addAddressToDatabase = (address: IAddress) => {
+    firebase.database().ref("/houses").child(address["street-address"])
+        .set(address)
+        .then(() => console.log(`Added ${address["street-address"]} to database`))
 }
